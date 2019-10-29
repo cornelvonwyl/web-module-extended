0f1457d445be97ca46d54d6b784c0b20e36624f6
cornelvonwyl <45622562+cornelvonwyl@users.noreply.github.com>
2019-10-29 18:09:49 +0100

    Scrolling

--- src/assets/js/object_load.js
+++ src/assets/js/object_load.js
@@ -13,26 +13,27 @@ import {
 } from './library/OBJLoader.js';
 
 
-
 var scene = new THREE.Scene();
 
 
 var camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 1000);
 // camera.position.set(0, 0, -120);
 
-camera.position.z = 200;
+
 scene.add(camera);
 
 
 var renderer = new THREE.WebGLRenderer({
-    alpha: true
+    alpha: true,
 });
 
 
+var canvascontainer = document.querySelector(".canvas_container");
 
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.querySelector(".container").appendChild(renderer.domElement);
+renderer.setSize(canvascontainer.offsetWidth, canvascontainer.offsetHeight);
+canvascontainer.appendChild(renderer.domElement);
+
 
 
 var controls = new OrbitControls(camera, renderer.domElement);
@@ -44,29 +45,38 @@ controls.autoRotateSpeed = 3;
 
 
 
-camera.position.set(0, 0, -300);
+camera.position.set(0, 0, 140);
 // controls.update();
 controls.enableZoom = false;
 
 
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+// var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+// keyLight.position.set(-100, 0, 100);
 
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 0, 100);
+// var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+// fillLight.position.set(100, 0, 100);
 
-var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
+// var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+// backLight.position.set(100, 0, -100).normalize();
 
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
+
+
+// scene.add(keyLight);
+// scene.add(fillLight);
+// scene.add(backLight);
+
+var light = new THREE.HemisphereLight(0xffffbb, 0x080820, 0.8);
+scene.add(light);
+
+var directionalLight = new THREE.DirectionalLight(0x000000, 0.2);
+scene.add(directionalLight);
+
 
 var mtlLoader = new MTLLoader();
 
 
-mtlLoader.load('/assets/images/texture.mtl', function (materials) {
+mtlLoader.load('/assets/images/heartGrande_red.mtl', function (materials) {
 
     materials.preload();
 
@@ -75,7 +85,7 @@ mtlLoader.load('/assets/images/texture.mtl', function (materials) {
     objLoader.setPath('/assets/images/');
 
 
-    objLoader.load('heartGrande.obj',
+    objLoader.load('heartGrande_red.obj',
         function (object) {
 
             scene.add(object);
